84bda97fea7a48b4b486a3208b130917a3de3c22
idasha.glenn7@gmail.com <idasha.glenn7@gmail.com>
2024-04-27 00:13:29 +0000

    Edited Products and Merch

--- client_code/Merch/__init__.py
+++ client_code/Merch/__init__.py
@@ -15,5 +15,10 @@ class Merch(MerchTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.loadProducts()
 
     # Any code you write here will run before the form opens.
+
+  def loadProducts(self):
+    products = anvil.server.call('getProductInfo')
+    print(products)
--- server_code/Products.py
+++ server_code/Products.py
@@ -8,15 +8,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
+@anvil.server.callable
+def getProductInfo():
+  #returns data that clients can read but not modify
+  return app_tables.products.client_readable()
