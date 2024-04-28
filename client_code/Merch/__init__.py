9fa0b14a4ef9c6675f3e36677b5c05586c716298
idasha.glenn7@gmail.com <idasha.glenn7@gmail.com>
2024-04-28 00:33:51 +0000

    Edited Merch and Home

--- client_code/Merch/__init__.py
+++ client_code/Merch/__init__.py
@@ -27,6 +27,3 @@ class Merch(MerchTemplate):
       c = Product(variant=product['variant'], button_text=f"Purchase for ${product['retailPrice']}", description=product['description'], image=product['image'], button_callback=None)
       product_panel.add_component(c, row=str(i//2), col_xs=6)
       self.container.add_component(product_panel)
-      
-
-      
