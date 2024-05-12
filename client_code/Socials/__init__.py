62ebf2755d6d07d137446e13e907179648922065
idasha.glenn7@gmail.com <idasha.glenn7@gmail.com>
2024-05-12 02:04:29 +0000

    Edited Landing and Socials

--- client_code/Socials/__init__.py
+++ client_code/Socials/__init__.py
@@ -17,6 +17,6 @@ class Socials(SocialsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    iframe = jQuery("<iframe width='100%' height='800px' style='border:solid 1px #777' frameborder='0' scrolling='no'>").attr("src", "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23ffffff&src=cmVhbC5hYm92ZXRoZWNsb3Vkc0BnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043")
+    iframe = jQuery("<iframe width='100%' height='800px' style='border:solid 1px #777' frameborder='0' scrolling='no'>").attr("src", "https://calendar.google.com/calendar/embed?src=real.abovetheclouds%40gmail.com&ctz=America%2FChicago")
     iframe.appendTo(get_dom_node(self.calcontainer))
     # Any code you write here will run before the form opens.
